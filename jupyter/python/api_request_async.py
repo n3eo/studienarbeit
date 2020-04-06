867ed1fcffde21716f5978e7f85280617a7c522a
Nico Rumsch <nico@notvads.ovh>
2020-04-06 14:18:43 +0200

    async implementation of api request

---
+++ jupyter/python/api_request_async.py
@@ -0,0 +1,18 @@
+import asyncio
+import logging
+import re
+import sys
+from typing import IO
+import urllib.error
+import urllib.parse
+
+import aiofiles
+import aiohttp
+from aiohttp import ClientSession
+
+logging.basicConfig(
+    format="%(asctime)s %(levelname)s:%(name)s: %(message)s",
+    level=logging.DEBUG,
+    datefmt="%H:%M:%S",
+    stream=sys.stderr,
+)
