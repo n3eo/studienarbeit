67359a6c93cd9a92ebb885a00c983918523b70dd
Nico Rumsch <nrumsch@t-online.de>
2020-05-04 18:46:11 +0000

    corrected imports and paths

--- python/main.py
+++ python/main.py
@@ -1,20 +1,22 @@
 from concurrent.futures import ProcessPoolExecutor
 from utils.api_request import process_json
+import logging
+from utils.db_connection import DbConnection as DBC
 
 if __name__ == "__main__":
     logging.basicConfig(
         format="%(asctime)s %(levelname)s:%(name)s: %(message)s",
         level=logging.INFO,
         datefmt="%H:%M:%S",
-        filename="../jupyter/logs/concurrent_db_ingestions.log",
+        filename="/app/logs/ingestion.log",
         filemode='a',
     )
 
-    workers = 128
+    workers = 64
     dbc = DBC()
     results = dbc.get_jsons()
     dbc.close()
 
     with ProcessPoolExecutor(max_workers=workers) as executor:
         for _ in executor.map(process_json, results):
-            pass
+            pass    
